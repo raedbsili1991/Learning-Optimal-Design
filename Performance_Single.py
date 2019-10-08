7ab76ec26d943cb48c369f590b55109fd1d2d41f
Raed Bsili <31764213+raedbsili1991@users.noreply.github.com>
2019-10-08 16:18:24 +0200

    Update Performance_Single.py

--- Performance_Single.py
+++ Performance_Single.py
@@ -1,4 +1,5 @@
 ## SYMBOLIC COMPUTATIONS OF FORWARD KINEMATICS FOR OMNIWIRST
+# Raed Bsili, Istituto Italiano di Tecnologia 
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D 
